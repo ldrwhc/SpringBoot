85d31c73707b256ca3cdcba427bfdfd82dff5ed8
ldrwhc <1355054240@qq.com>
2021-05-22 17:24:40 +0800

    new file

---
+++ src/main/java/com/njupt/whc/WhcApplication.java
@@ -0,0 +1,13 @@
+package com.njupt.whc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhcApplication.class, args);
+    }
+
+}
